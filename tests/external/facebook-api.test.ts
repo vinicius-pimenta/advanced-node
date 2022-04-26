f595fd10b30008b4ac208226ef5e014581046c3e
Vinicius Pimenta <viniciustavarespimenta@gmail.com>
2022-04-26 16:48:31 -0300

    ensure FacebookApi returns undefined when HttpGetClient throws

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
-    expect(1).toBe(1)
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAANaXT8ZBhKQBAA5QmsBUvWNAqb4KVfyUpp5qMymXBahMZCGIgfjmghH7sAEFosRSZAWElzjgPnjcok8YLGZALF6d1UDP2b6AO9A9iBOZAOpJmCI3fZBhN9edNHVcahWjV3DhMZCfY51ZCf7LpeCBPdziqN9xbuqpKZB5tgRrNduMvZBnNdYTcsSu7TTHlxLvmQHExfdFovDfrD31igAMdKMzr' })
+
+    expect(fbUser).toEqual({
+      facebookId: '106876265345044',
+      name: 'Vinicius Teste',
+      email: 'vinicius_opafnfd_teste@tfbnw.net'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
