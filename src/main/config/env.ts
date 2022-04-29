54cafe5e6e0a14d09a3881cc54dfcf47cef86dab
Vinicius Pimenta <viniciustavarespimenta@gmail.com>
2022-04-29 09:31:02 -0300

    ensure ExpressMiddleware calls handle with correct request

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,9 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '943781469652132',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '3c7840a2f78abeb4c237520080cae545'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
+    accessToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? '3jk24h32jk4h'
+  jwtSecret: process.env.JWT_SECRET ?? ''
 }
